13f8d3dbec5517ee2ff3453683dfa5cb7d33f827
Bekzhan Satiev <109339205+tarantulDeveloper@users.noreply.github.com>
2022-09-19 20:59:11 +0600

    Create Car.java

---
+++ classesTasks/com/company/vehicles/Car.java
@@ -0,0 +1,87 @@
+package com.company.vehicles;
+
+import com.company.details.Engine;
+import com.company.professions.Driver;
+
+public class Car {
+    private String model;
+    private String carClass;
+    private int weight;
+    private Driver driver;
+    private Engine engine;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarClass() {
+        return carClass;
+    }
+
+    public void setCarClass(String carClass) {
+        this.carClass = carClass;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Car(String model, String carClass, int weight, Driver driver, Engine engine) {
+        this.model = model;
+        this.carClass = carClass;
+        this.weight = weight;
+        this.driver = driver;
+        this.engine = engine;
+    }
+
+    void start() {
+        System.out.println("Поехали");
+    }
+
+    void stop() {
+        System.out.println("Останавливаемся");
+    }
+
+    void turnRight() {
+        System.out.println("Поворот направо");
+    }
+
+    void turnLeft() {
+        System.out.println("Поворот налево");
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", carClass='" + carClass + '\'' +
+                ", weight=" + weight +
+                ", driver=" + driver +
+                ", engine=" + engine +
+                '}';
+    }
+}
