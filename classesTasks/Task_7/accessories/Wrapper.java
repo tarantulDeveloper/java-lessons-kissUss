f390d7051e882468a1a84356ad842eea056201eb
Bekzhan Satiev <109339205+tarantulDeveloper@users.noreply.github.com>
2022-09-19 23:50:08 +0600

    Create Wrapper.java

---
+++ classesTasks/Task_7/accessories/Wrapper.java
@@ -0,0 +1,13 @@
+package task_7.accessories;
+
+import task_7.Accessory;
+
+public class Wrapper extends Accessory {
+  public Wrapper(int accessoryPrice) {
+      super(accessoryPrice);
+  }
+
+  public Wrapper() {
+      super(80);
+  }
+}
