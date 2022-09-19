e6c92946660efbedc5cccaaddd13338d6bffaadd
Bekzhan Satiev <109339205+tarantulDeveloper@users.noreply.github.com>
2022-09-19 23:49:27 +0600

    Create Cologne.java

---
+++ classesTasks/Task_7/accessories/Cologne.java
@@ -0,0 +1,14 @@
+package task_7.accessories;
+
+import task_7.Accessory;
+
+public class Cologne extends Accessory {
+
+    public Cologne(int accessoryPrice) {
+        super(accessoryPrice);
+    }
+
+    public Cologne() {
+        super(150);
+    }
+}
