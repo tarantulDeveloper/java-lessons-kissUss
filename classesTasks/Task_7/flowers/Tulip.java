6e09c147fbcb4c043e4b867399711592be9904b2
Bekzhan Satiev <109339205+tarantulDeveloper@users.noreply.github.com>
2022-09-19 23:51:44 +0600

    Create Tulip.java

---
+++ classesTasks/Task_7/flowers/Tulip.java
@@ -0,0 +1,10 @@
+package task_7.flowers;
+
+import task_7.Flower;
+
+public class Tulip extends Flower {
+
+    public Tulip(int freshness, int price, double stemLength) {
+        super(freshness, price, stemLength);
+    }
+}
