592ebfe08c2c91971524b1845d6f197bc6070a67
Bekzhan Satiev <109339205+tarantulDeveloper@users.noreply.github.com>
2022-09-19 23:50:46 +0600

    Create Petunia.java

---
+++ classesTasks/Task_7/flowers/Petunia.java
@@ -0,0 +1,9 @@
+package task_7.flowers;
+
+import task_7.Flower;
+
+public class Petunia extends Flower {
+    public Petunia(int freshness, int price, double stemLength) {
+        super(freshness, price, stemLength);
+    }
+}
