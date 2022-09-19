9742d33e0b734461d84569e117848b1924974019
Bekzhan Satiev <109339205+tarantulDeveloper@users.noreply.github.com>
2022-09-19 21:00:27 +0600

    Create Engine.java

---
+++ classesTasks/com/company/details/Engine.java
@@ -0,0 +1,27 @@
+package com.company.details;
+
+public class Engine {
+    private int power;
+    private String manufacturer;
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Engine(int power, String manufacturer) {
+        this.power = power;
+        this.manufacturer = manufacturer;
+    }
+}
