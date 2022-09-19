acd0f3174e2c473196ddbdfe4cda537354ec6d7d
Bekzhan Satiev <109339205+tarantulDeveloper@users.noreply.github.com>
2022-09-19 23:51:15 +0600

    Create Rose.java

---
+++ classesTasks/Task_7/flowers/Rose.java
@@ -0,0 +1,11 @@
+package task_7.flowers;
+
+import task_7.Flower;
+
+public class Rose extends Flower {
+    public Rose(int freshness, int price, double stemLength) {
+        super(freshness, price, stemLength);
+    }
+
+
+}
